6e5096d6810f7a0df2ea9acf9bfb7dab5ddb894d
moth96 <43572813+moth96@users.noreply.github.com>
2020-09-01 09:23:20 +0800

    Add files via upload

---
+++ 486预测赢家.cpp
@@ -0,0 +1,15 @@
+#include"solution.h"
+
+bool PredictTheWinner(vector<int>& nums) {
+	int n = nums.size();
+	vector<vector<int>> dp(n, vector<int>(n));
+	for (int i = n - 1; i >= 0; i--)
+	{
+		for (int j = i; j < n; j++)
+		{
+			if (i == j) dp[i][i] = nums[i];
+			else dp[i][j] = max((nums[i] - dp[i + 1][j]), nums[j] - dp[i][j - 1]);
+		}
+	}
+	return dp[0][n - 1] >= 0;
+}
