a051b601404d7a1265295254292df78db62cb856
moth96 <43572813+moth96@users.noreply.github.com>
2020-09-06 10:06:23 +0800

    Add files via upload

---
+++ 0102.二叉树的层序遍历.cpp
@@ -0,0 +1,24 @@
+#include"solution.h"
+
+vector<vector<int>> levelOrder(TreeNode* root)
+{
+	vector<vector<int>> res;
+	queue<TreeNode *> q;
+
+	if (!root) return res;
+	q.push(root);
+	while (!q.empty())
+	{
+		int length = q.size();
+		res.push_back(vector<int>());
+		for (int i = 0; i < length; i++)
+		{
+			TreeNode* temp = q.front();
+			q.pop();
+			res.back().push_back(temp->val);
+			if (temp->left) q.push(temp->left);
+			if (temp->right) q.push(temp->right);
+		}
+	}
+	return res;
+}
