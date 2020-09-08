9e8ab63f9e8a94e3ad24063361a980e64c5e61b5
moth96 <43572813+moth96@users.noreply.github.com>
2020-09-08 15:39:56 +0800

    Add files via upload

---
+++ 0114.二叉树展开为链表.cpp
@@ -0,0 +1,21 @@
+#include"solution.h"
+#include"tree.h"
+
+void flatten(TreeNode* root) {
+    vector<TreeNode*> l;
+    preorderTraversal(root, l);
+    int n = l.size();
+    for (int i = 1; i < n; i++) {
+        TreeNode* prev = l.at(i - 1), * curr = l.at(i);
+        prev->left = nullptr;
+        prev->right = curr;
+    }
+}
+
+void preorderTraversal(TreeNode* root, vector<TreeNode*>& l) {
+    if (root != NULL) {
+        l.push_back(root);
+        preorderTraversal(root->left, l);
+        preorderTraversal(root->right, l);
+    }
+}
