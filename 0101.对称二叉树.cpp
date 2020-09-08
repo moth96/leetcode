834028e6cdc19d32c5b2c0dbdf6fdbe054848043
moth96 <43572813+moth96@users.noreply.github.com>
2020-09-08 15:24:30 +0800

    Add files via upload

---
+++ 0101.对称二叉树.cpp
@@ -0,0 +1,12 @@
+#include"solution.h"
+#include"tree.h"
+
+bool check(TreeNode* p, TreeNode* q) {
+    if (!p && !q) return true;
+    if (!p || !q) return false;
+    return p->val == q->val && check(p->left, q->right) && check(p->right, q->left);
+}
+
+bool isSymmetric(TreeNode* root) {
+    return check(root, root);
+}
