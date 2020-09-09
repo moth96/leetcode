420730d7ea2f6cd2cee5b6d5785976b1fa6ca991
moth96 <43572813+moth96@users.noreply.github.com>
2020-09-09 17:01:13 +0800

    Add files via upload

---
+++ 0160.相交链表.cpp
@@ -0,0 +1,14 @@
+#include"solution.h"
+#include"list.h"
+
+ListNode* getIntersectionNode(ListNode* headA, ListNode* headB)
+{
+	ListNode* q = headA;
+	ListNode* p = headB;
+	while (q != p)
+	{
+		q = q == NULL ? headB : q->next;
+		p = p == NULL ? headA : p->next;
+	}
+	return q;
+}
