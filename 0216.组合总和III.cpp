70ffa52335e0c0c7840d0285a70692dd9ff6b133
moth96 <43572813+moth96@users.noreply.github.com>
2020-09-11 12:46:43 +0800

    Update 0216.组合总和III.cpp

--- 0216.组合总和III.cpp
+++ 0216.组合总和III.cpp
@@ -1,36 +1,35 @@
 #include"solution.h"
 
+//step 1
+vector<vector<int>> res;
+vector<int> cur;
 
-//解法1
-//vector<vector<int>> res;
-//vector<int> cur;
-//
-//void backtrack(int k, int n,int i)
-//{
-//	if (k == 0)
-//	{
-//		if (n == 0)
-//		{
-//			res.push_back(cur);
-//			return;
-//		}
-//		else
-//			return;
-//	}
-//	if (i > 9) return;
-//	cur.push_back(i);
-//	backtrack(k - 1, n - i, i + 1);
-//	cur.pop_back();
-//	backtrack(k, n, i + 1);
-//}
-//
-//vector<vector<int>> combinationSum3(int k, int n)
-//{
-//	backtrack(k, n, 1);
-//	return res;
-//}
+void backtrack(int k, int n,int i)
+{
+	if (k == 0)
+	{
+		if (n == 0)
+		{
+			res.push_back(cur);
+			return;
+		}
+		else
+			return;
+	}
+	if (i > 9) return;
+	cur.push_back(i);
+	backtrack(k - 1, n - i, i + 1);
+	cur.pop_back();
+	backtrack(k, n, i + 1);
+}
 
-//解法2
+vector<vector<int>> combinationSum3(int k, int n)
+{
+	backtrack(k, n, 1);
+	return res;
+}
+
+//step2
 //vector<vector<int>> res;
 //vector<int> cur;
 //
