2d3a2f302b40a0a8387befe8015fee90e34e23bb
moth96 <43572813+moth96@users.noreply.github.com>
2020-09-11 12:44:05 +0800

    Add files via upload

---
+++ 0216.组合总和III.cpp
@@ -0,0 +1,60 @@
+#include"solution.h"
+
+
+//解法1
+//vector<vector<int>> res;
+//vector<int> cur;
+//
+//void backtrack(int k, int n,int i)
+//{
+//	if (k == 0)
+//	{
+//		if (n == 0)
+//		{
+//			res.push_back(cur);
+//			return;
+//		}
+//		else
+//			return;
+//	}
+//	if (i > 9) return;
+//	cur.push_back(i);
+//	backtrack(k - 1, n - i, i + 1);
+//	cur.pop_back();
+//	backtrack(k, n, i + 1);
+//}
+//
+//vector<vector<int>> combinationSum3(int k, int n)
+//{
+//	backtrack(k, n, 1);
+//	return res;
+//}
+
+//解法2
+//vector<vector<int>> res;
+//vector<int> cur;
+//
+//void backtrack(int k, int n, int start)
+//{
+//	if (k == 0)
+//	{
+//		if (n == 0)
+//		{
+//			res.push_back(cur);
+//			return;
+//		}
+//		else return;
+//	}
+//	for (int i = start; i < 10; i++)
+//	{
+//		cur.push_back(i);
+//		backtrack(k - 1, n - i, i + 1);
+//		cur.pop_back();
+//	}
+//}
+//
+//vector<vector<int>> combinationSum3(int k, int n)
+//{
+//	backtrack(k, n, 1);
+//	return res;
+//}
