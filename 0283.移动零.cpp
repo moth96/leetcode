bbb3604d7fe841b7c7277527262dff77a88362ff
moth96 <43572813+moth96@users.noreply.github.com>
2020-09-19 16:12:18 +0800

    Add files via upload

---
+++ 0283.移动零.cpp
@@ -0,0 +1,9 @@
+#include"solution.h"
+
+void moveZeroes(vector<int>& nums) 
+{
+    for (int i = 0, j = 0; j < nums.size(); j++) 
+    {
+        if (nums[j] != 0)   swap(nums[i++], nums[j]);
+    }
+}
