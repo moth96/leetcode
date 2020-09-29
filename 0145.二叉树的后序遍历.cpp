a8853d40e107de9b646eb0fd2526cbd7ae2a78ca
moth96 <43572813+moth96@users.noreply.github.com>
2020-09-29 20:41:12 +0800

    Add files via upload

---
+++ 0145.二叉树的后序遍历.cpp
@@ -0,0 +1,17 @@
+#include"solution.h"
+#include"tree.h"
+
+void dfs(TreeNode* root, vector<int>& res)
+{
+	if (root == nullptr) return;
+	dfs(root->left, res);
+	dfs(root->right, res);
+	res.push_back(root->val);
+}
+
+vector<int> postorderTraversal(TreeNode* root)
+{
+	vector<int>res;
+	dfs(root, res);
+	return res;
+}
