c85fa2a0dea46596003d0f5fac113018b8a9f0bd
moth96 <43572813+moth96@users.noreply.github.com>
2020-10-15 13:51:29 +0800

    Update 0003.无重复最长子串.cpp

--- 0003.无重复最长子串.cpp
+++ 0003.无重复最长子串.cpp
@@ -26,3 +26,22 @@ int lengthOfLongestSubstring(string s) {
 		maxLen = curLen;
 	return maxLen;
 }
+
+int lengthOfLongestSubstring(string s) 
+    {
+        if(s.size() == 0) return 0;
+        unordered_set<char> st;
+        int res = 0;
+        int left = 0;
+        for(int i = 0; i < s.size(); i++)
+        {
+            while(st.find(s[i]) != st.end())
+            {
+                st.erase(s[left]);
+                left++;
+            }
+            res = max(res,i - left + 1);
+            st.insert(s[i]);
+        }
+        return res;
+    }
