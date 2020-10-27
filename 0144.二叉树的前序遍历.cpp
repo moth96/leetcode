0c085e7140aab50ed6a3f53a2093a6a4195654ab
moth96 <312041716@qq.com>
2020-10-27 22:23:58 +0800

    add by git

---
+++ 0144.二叉树的前序遍历.cpp
@@ -0,0 +1,10 @@
+#include "solution.h"
+
+vector<int> res;
+vector<int> preorderTraversal(TreeNode* root)
+{
+	if (!root) return res;
+	res.push_back(root->val);
+	preorderTraversal(root->left);
+	preorderTraversal(root->right);
+}
