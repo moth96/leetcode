cbc1530836e8e7ca422b6c0f15eede675ca4bb2d
moth96 <312041716@qq.com>
2021-05-12 10:24:33 +0800

    Leetcode Daily Question 20210512

---
+++ 1310.子数组异或查询.cpp
@@ -0,0 +1,13 @@
+vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
+    int n = arr.size();
+    vector<int> xors(n + 1);
+    for (int i = 0; i < n; i++) {
+        xors[i + 1] = xors[i] ^ arr[i];
+    }
+    int m = queries.size();
+    vector<int> ret(m);
+    for (int i = 0; i < m; i++) {
+        ret[i] = xors[queries[i][0]] ^ xors[queries[i][1] + 1];
+    }
+    return ret;
+}
